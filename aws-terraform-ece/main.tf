6b40bd53f5c1cc2e2a3e4d0748db8feeee12473d
ProgrammingProphet <adityasraazvishwakarma@gmail.com>
2026-04-02 11:19:36 +0530

    basic updates

--- aws-terraform-ece/main.tf
+++ aws-terraform-ece/main.tf
@@ -12,16 +12,16 @@ data "aws_subnets" "default" {
 }
 
 # Get latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+# data "aws_ami" "default-ami" {
+#   most_recent = true
 
-  owners = ["amazon"]
+#   owners = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
 
 # Create Security Group
 resource "aws_security_group" "ec2_sg" {
@@ -34,7 +34,8 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ⚠️ open to all (for learning)
+    # cidr_blocks = ["0.0.0.0/0"] # ⚠️ open to all (for learning)
+    cidr_blocks = [var.my_ip]
   }
 
   egress {
@@ -53,7 +54,8 @@ resource "aws_key_pair" "my_key" {
 
 # Create EC2 instance
 resource "aws_instance" "ec2" {
-  ami                    = data.aws_ami.amazon_linux.id
+#   ami                    = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami                    = "ami-053b0d53c279acc90" #Ubuntu 22.04 LTS
   instance_type          = "t3.micro"
   subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
